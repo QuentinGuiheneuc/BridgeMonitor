760638b60a275f90f83412848227bc9881402318
QuentinGuiheneuc <guiheneuc.quentin@outlook.fr>
2021-04-01 10:00:40 +0200

    init_projet

---
+++ AppPontChaban/Controllers/ToutesLesFermeturesController.cs
@@ -0,0 +1,37 @@
+﻿using AppPontChaban.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppPontChaban.Controllers
+{
+    public class ToutesLesFermeturesController : Controller
+    {
+        private readonly ILogger<ToutesLesFermeturesController> _logger;
+
+        public ToutesLesFermeturesController(ILogger<ToutesLesFermeturesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
